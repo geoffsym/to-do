b0ad7d89ced09224571369a991a64bb8c288f4b5
Geoff Symonds <gsymonds@paychex.com>
2023-10-18 16:45:12 -0400

    getting user input

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
