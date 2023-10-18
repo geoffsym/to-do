23688358451b429e41d4b4facb8d9fca6af6568b
Geoff Symonds <gsymonds@paychex.com>
2023-10-18 17:31:46 -0400

    installing angular material

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
