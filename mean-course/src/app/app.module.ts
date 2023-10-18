f408ed6472d56f59d6308bf626555361a1a4f428
Geoff Symonds <gsymonds@paychex.com>
2023-10-18 16:26:57 -0400

    adding our first component

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostCreateComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
