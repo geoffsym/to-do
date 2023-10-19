ea9ef4a7f04d747936fc15f61d3c095bdd16dd1f
Geoff Symonds <gsymonds@paychex.com>
2023-10-19 16:34:46 -0400

    adding a header

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
+  declarations: [AppComponent, HeaderComponent, PostCreateComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
@@ -17,6 +19,7 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     MatButtonModule,
     MatCardModule,
     MatInputModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
