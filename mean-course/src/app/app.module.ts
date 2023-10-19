acad3f2619db52c7579255bbc2d666315e07b01f
Geoff Symonds <gsymonds@paychex.com>
2023-10-19 17:05:59 -0400

    outputting posts

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -5,19 +5,27 @@ import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, PostCreateComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PostCreateComponent,
+    PostListComponent,
+  ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     MatButtonModule,
     MatCardModule,
+    MatExpansionModule,
     MatInputModule,
     MatToolbarModule,
   ],
