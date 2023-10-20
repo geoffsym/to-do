1f450e2cc37a4dbd35d490ae384180e9d32f5477
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-20 12:49:23 -0400

    adding an image preview

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -13,6 +13,7 @@ import { PostsService } from '../posts.service';
 export class PostCreateComponent implements OnInit {
   public isLoading = false;
   public form: FormGroup;
+  public imagePreview: string;
   public enteredTitle = '';
   public enteredContent = '';
   public post: Post;
@@ -65,8 +66,11 @@ export class PostCreateComponent implements OnInit {
     const file = (event.target as HTMLInputElement).files[0];
     this.form.patchValue({ image: file });
     this.form.get('image').updateValueAndValidity();
-    console.log(file);
-    console.log(this.form);
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
