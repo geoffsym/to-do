080e9ed67042aac121523787afa72a3e45723ceb
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-20 00:25:42 -0400

    fetching data from a database

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -44,21 +44,11 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "sdafcevrsaf",
-      title: "First Server-Side Post",
-      content: "This is coming from the server",
-    },
-    {
-      id: "tiuhjhgnmhj",
-      title: "Second Server-Side Post",
-      content: "This is also coming from the server",
-    },
-  ];
-  res.status(200).json({
-    message: "posts fetched succesfully!",
-    posts: posts,
+  Post.find().then((posts) => {
+    res.status(200).json({
+      message: "posts fetched succesfully!",
+      posts: posts,
+    });
   });
 });
 
