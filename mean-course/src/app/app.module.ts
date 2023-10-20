aa8e0ba0f1820ed5eff519eb31bfd42ea3e2f1e8
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-20 10:13:42 -0400

    adding routing

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -21,6 +22,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     PostListComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
