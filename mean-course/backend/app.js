91707accbf14c2a3af07421a62e0c588d7b6e493
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-20 11:41:03 -0400

    reorganizing backend routes

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,8 +1,8 @@
-import Post from "./models/post.js";
 import dotenv from "dotenv";
 import dotenvExpand from "dotenv-expand";
 import express from "express";
 import mongoose from "mongoose";
+import postRouter from "./routes/posts.js";
 
 dotenvExpand.expand(dotenv.config());
 
@@ -32,52 +32,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: "Post added successfully",
-      postId: createdPost._id,
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Update seccessful!" });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((posts) => {
-    res.status(200).json({
-      message: "posts fetched succesfully!",
-      posts: posts,
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) res.status(200).json(post);
-    else res.status(404).json({ message: "Post not found!" });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
-  });
-});
+app.use("/api/posts", postRouter);
 
 export default app;
