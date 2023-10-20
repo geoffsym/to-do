759e3b8ecc901bc31349d39a7f4cf1854f81ff02
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-19 23:11:22 -0400

    adding the post backend point

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -13,6 +15,14 @@ app.use((req, res, next) => {
     "GET, POST, PATCH, DELETE, OPTIONS"
   );
   next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully",
+  });
 });
 
 app.use("/api/posts", (req, res, next) => {
