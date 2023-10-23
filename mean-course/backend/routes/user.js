00ae8cc858707e70d23db81d5ada6d37e540cad7
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-23 09:29:31 -0400

    creating a new user upon request

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -1,7 +1,27 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/signup", (req, res, next) => {});
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    user
+      .save()
+      .then((result) => {
+        res.status(200).json({
+          message: "User created!",
+          result: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
+  });
+});
 
 export default router;
