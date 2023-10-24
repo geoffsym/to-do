7317b4a413e9a1354e1ad48c9d86ecdf01e61bdf
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-24 14:32:29 -0400

    the error interceptor

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { ErrorInterceptor } from './error-interceptor';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './auth/login/login.component';
 import { MatButtonModule } from '@angular/material/button';
@@ -48,6 +49,11 @@ import { SignupComponent } from './auth/signup/signup.component';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
       multi: true,
     },
   ],
