282fdf55b32dbe665d56de856be7f1711a85f29a
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-24 09:41:28 -0400

    adding a reference to the model

--- mean-course/backend/models/post.js
+++ mean-course/backend/models/post.js
@@ -4,6 +4,11 @@ const postSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   imagePath: { type: String, requred: true },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 export default mongoose.model("Post", postSchema);
