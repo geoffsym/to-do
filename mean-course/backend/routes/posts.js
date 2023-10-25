c70def8b734eaa31cd0cf3f231ade09c1e24b20f
Geoff Symonds <geoff.sym93@gmail.com>
2023-10-24 21:52:39 -0400

    separating the middleware

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -8,45 +8,13 @@ import {
 
 import { checkAuth } from "../middleware/check-auth.js";
 import express from "express";
-import multer from "multer";
+import extractFile from "../middleware/file.js";
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-};
+router.post("/", checkAuth, extractFile, createPost);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  },
-});
-
-router.post(
-  "/",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  createPost
-);
-
-router.put(
-  "/:id",
-  checkAuth,
-  multer({ storage: storage }).single("image"),
-  updatePost
-);
+router.put("/:id", checkAuth, extractFile, updatePost);
 
 router.get("/", getPosts);
 
